381a70657db71516bd4a498f7f8bd84510ad759d
0eff <65223348+0eff@users.noreply.github.com>
2020-05-13 15:06:25 +0700

    up

---
+++ ps.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import psutil
+import time
+
+def getProcess(pName):
+    all_pids  = psutil.pids()
+
+    process_lst = []
+    for pid in all_pids:
+        p = psutil.Process(pid)
+        if (p.name() == pName):
+            process_lst.append(p)
+
+    return process_lst
+
+name_str="barad_agent,prometheus,node_exporter,grafana-server"
+
+#---------------cpu
+print '# TYPE process_cpu_percent gauge'
+
+for name in name_str.split(','):
+  p_lst = getProcess(name)
+
+  # Get cpu used percent of processes in top flush time
+  t_list = []
+  n=0
+  for p in p_lst:
+    for i in range(50):
+        p_cpu = p.cpu_percent(interval=0.1)
+        t_list.append(p_cpu)
+
+    p_memory = round(p.memory_percent(),3)
+
+    if(len(p_lst)==1):
+      print 'process_cpu_percent{mode="%s"} %s'%(name,round(float(sum(t_list))/len(t_list),3))
+    else:
+      n=n+1
+      print 'process_cpu_percent{mode="%s_%s"} %s'%(name,n,round(float(sum(t_list))/len(t_list),3))
+
+#---------------memory
+print ''
+print '# TYPE process_memory_percent gauge'
+
+for name in name_str.split(','):
+  p_lst = getProcess(name)
+
+  # Get memory used percent of processes in top flush time
+  t_list = []
+  n=0
+  for p in p_lst:
+    p_memory = round(p.memory_percent(),3)
+
+    if(len(p_lst)==1):
+      print 'process_memory_percent{mode="%s"} %s'%(name,p_memory)
+    else:
+      n=n+1
+      print 'process_memory_percent{mode="%s_%s"} %s'%(name,n,p_memory)
