bfc779148ea09f71131b6c0da818c2983e9e2182
0eff <65223348+0eff@users.noreply.github.com>
2020-05-15 15:30:28 +0700

    up

--- install_cli.sh
+++ install_cli.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 #
 
-# install promethus client and the cpu and memory percent script for a single process 
+# by jude
+
+# install promethus client and the cpu and memory percent script for a single process
 
 rm -f /usr/local/src/*_exporter*tar.gz
 rm -rf /usr/local/*_exporter/
+
+# install node_exporter
 
 cd /usr/local/src/ && wget https://github.com/prometheus/node_exporter/releases/download/v1.0.0-rc.0/node_exporter-1.0.0-rc.0.linux-amd64.tar.gz
 tar xf node_exporter-1.0.0-rc.0.linux-amd64.tar.gz -C /usr/local/node_exporter/
@@ -31,6 +35,8 @@ systemctl enable node_exporter
 systemctl start node_exporter
 systemctl status node_exporter
 
+# install blackbox_exporter
+
 cd /usr/local/src/ && wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.16.0/blackbox_exporter-0.16.0.linux-amd64.tar.gz
 tar xf blackbox_exporter-0.16.0.linux-amd64.tar.gz -C /usr/local/blackbox_exporter/
 cat <<EOF  > /lib/systemd/system/blackbox_exporter.service
@@ -53,6 +59,32 @@ systemctl enable blackbox_exporter
 systemctl start blackbox_exporter
 systemctl status blackbox_exporter
 
+# install mongodb_exporter
+
+mkdir /usr/local/mongodb_exporter/ -p
+cd /usr/local/mongodb_exporter/ && wget https://raw.githubusercontent.com/0eff/prometheus_grafana/master/mongodb_exporter_v0.11.0/mongodb_exporter
+cat <<EOF  > /lib/systemd/system/mongodb_exporter.service
+[Unit]
+Description=mongodb exporter server
+After=network.target
+
+[Service]
+ExecStart=/usr/local/mongodb_exporter/mongodb_exporter --mongodb.uri=mongodb://mongodb_exporter:password@127.0.0.1:27017
+KillMode=process
+Restart=on-failure
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+Alias= mongodb_exporter.service
+EOF
+
+systemctl daemon-reload
+systemctl enable mongodb_exporter
+systemctl start mongodb_exporter
+systemctl status mongodb_exporter
+
+# crontab
 
 yum install python-devel python2-pip.noarch -y
 pip2 install psutil
---
+++ mongodb_exporter_v0.11.0/mongodb_exporter_install.sh
@@ -0,0 +1,31 @@
+# install mongodb_exporter
+
+#
+#bind_ip=127.0.0.1,172.19.xx.xx
+#
+#auth=true
+#keyFile=/data/mongodb/qm_mongo.key
+#
+
+mkdir /usr/local/mongodb_exporter/ -p
+cd /usr/local/mongodb_exporter/ && wget https://raw.githubusercontent.com/0eff/prometheus_grafana/master/mongodb_exporter_v0.11.0/mongodb_exporter
+cat <<EOF  > /lib/systemd/system/mongodb_exporter.service
+[Unit]
+Description=mongodb exporter server
+After=network.target
+
+[Service]
+ExecStart=/usr/local/mongodb_exporter/mongodb_exporter --mongodb.uri=mongodb://mongodb_exporter:password@127.0.0.1:27017
+KillMode=process
+Restart=on-failure
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+Alias=mongodb_exporter.service
+EOF
+
+systemctl daemon-reload
+systemctl enable mongodb_exporter
+systemctl start mongodb_exporter
+systemctl status mongodb_exporter
